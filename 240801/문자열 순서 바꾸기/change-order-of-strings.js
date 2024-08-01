0216c5042f358e4c77586c3a31e013311e766768
HHHOOOXX <165631381+HHHOOOXX@users.noreply.github.com>
2024-08-01 20:57:30 +0900

    Title: 문자열 순서 바꾸기, Time: 188ms, Memory: 15MB, Status: Passed - CodeTree

--- 240801/문자열 순서 바꾸기/change-order-of-strings.js
+++ 240801/문자열 순서 바꾸기/change-order-of-strings.js
@@ -1,6 +1,10 @@
 const fs = require("fs");
 let input = fs.readFileSync(0).toString().trim().split("\n");
-let t = String(input[0]);
-let s = String(input[1]);
+
+// 문자열 순서 변경
+let s = String(input[0]);
+let t = String(input[1]);
+
+// 출력
 console.log(t);
 console.log(s);
