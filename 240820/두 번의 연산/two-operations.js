f77b1206896eba840e5676c9d0699516e336e0da
HHHOOOXX <165631381+HHHOOOXX@users.noreply.github.com>
2024-08-20 21:08:05 +0900

    Title: 두 번의 연산, Time: 172ms, Memory: 47MB, Status: Passed - CodeTree

---
+++ 240820/두 번의 연산/two-operations.js
@@ -0,0 +1,12 @@
+const fs = require("fs");
+let input = Number(fs.readFileSync(0).toString().trim());
+
+let a = input;
+
+if (a % 2 === 1) {
+    a += 3;
+}
+if (a % 3 === 0) {
+    a /= 3;
+}
+console.log(a);
